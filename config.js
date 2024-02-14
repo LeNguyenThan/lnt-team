696a1bfb9607a9e2e9d6e0d864ceb0de7ec45669
LeNguyenThan <159506468+LeNguyenThan@users.noreply.github.com>
2024-02-15 05:51:43 +0700

    Create config.js
    Upload by lnt

---
+++ config.js
@@ -0,0 +1,55 @@
+{
+    "language": "vi",
+    "DeveloperMode": false,
+    "autoCreateDB": true,
+    "notiGroup": true,
+    "iconUnsend": {
+        "status": true,
+        "icon": "😠"
+    },
+    "ndhOnly": false,
+    "adminOnly": false,
+    "NOTIFICATION": true,
+    "allowInbox": false,
+    "autoClean": true,
+    "adminPaseOnly": true,
+    "encryptSt": false,
+    "UPTIME": 0,
+    "DASHBOARD": false,
+    "commandDisabled": [],
+    "eventDisabled": [],
+    "EMAIL": "",
+    "PASSWORD": "",
+    "OTPKEY": "",
+    "BOTNAME": "Suzu - Bot",
+    "PREFIX": "/",
+    "NDH": [
+        "100077749897356",
+        " 61551930773171"
+    ],
+    "ADMINBOT": [
+        "100077749897356",
+        "61551930773171"
+    ],
+    "DATABASE": {
+        "sqlite": {
+            "storage": "data.sqlite"
+        }
+    },
+    "APPSTATEPATH": "appstate.json",
+    "NPM_FCA": "fca-horizon-remake",
+    "SETUP_WIN": {
+        "port": 8888,
+        "use": "Nếu chạy nhiều hơn 1 file trên win thì thay đổi số này ở file thứ 2, 3, 4..."
+    },
+    "FCAOption": {
+        "forceLogin": true,
+        "listenEvents": true,
+        "pauseLog": true,
+        "logLevel": "silent",
+        "selfListen": true,
+        "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"
+    },
+    "version": "1.2.14",
+    "audio": {
+}
